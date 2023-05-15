f21960d855b851ad0d125e1bcb4bbff411933420
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2023-05-15 19:27:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 624 ms, Memory: 68132 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+N,M,R = map(int,input().split())
+lst = [[]for i in range(N+1)]
+cnt = 1
+visited=[0]*(N+1)
+for i in range(M):
+    a,b=map(int,input().split())
+    lst[a].append(b)
+    lst[b].append(a)
+for j in lst:
+    j.sort()
+
+def dfs (st):
+    global cnt
+    visited[st]=cnt
+    for i in lst[st]:
+        if visited[i] == 0:
+            cnt+=1
+            dfs(i)
+dfs(R)
+for i in range(1,N+1):
+    print(visited[i])
