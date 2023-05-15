acb360a06c38815762084034dfd291575bbcd86c
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2023-05-15 19:28:30 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 684 ms, Memory: 58420 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N,M,R = map(int,input().split())
+lst = [[]for _ in range(N+1)]
+visited = [0]*(N+1)
+cnt = 1
+for i in range(M):
+    a,b=map(int,input().split())
+    lst[a].append(b)
+    lst[b].append(a)
+for i in lst:
+    i.sort(reverse=True)
+
+def bfs (st):
+    global cnt
+    que = deque()
+    que.append(st)
+    visited[st] = 1
+    while que:
+        a = que.popleft()
+        for i in lst[a]:
+            if visited[i]==0:
+                cnt+=1
+                visited[i]=cnt
+                que.append(i)
+bfs(R)
+for i in range(1,N+1):
+    print(visited[i])
+
+
