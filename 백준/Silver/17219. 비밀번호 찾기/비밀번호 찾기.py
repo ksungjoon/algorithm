66b8dc049da6ef51265727fe64f579496b7fdd41
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2023-06-28 15:41:55 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 228 ms, Memory: 49344 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+MEMO = {}
+
+for n in range(N):
+    key, value = input().split()
+    MEMO[key] = value
+
+for m in range(M):
+    key = input().strip()
+    print(MEMO[key])
