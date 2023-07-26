7bce99d89f53cfc4804b834d4fdcdd4b04b2858d
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2023-07-26 22:16:06 +0900

    [level 2] Title: 타겟 넘버, Time: 5.08 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,21 @@
+
+    
+def solution(numbers, target):
+    # global c
+    answer = 0
+    dfs(0,0,numbers,target)
+    answer = c
+        
+    return answer
+c = 0
+def dfs(lev,Sum,lst,num):
+    global c
+    if lev == len(lst):
+        if Sum == num:
+            c += 1
+        return
+    for i in range(2):
+        if i ==1:
+            dfs(lev+1,Sum+lst[lev],lst,num)
+        else:
+            dfs(lev+1,Sum-lst[lev],lst,num)   
