0520947cb116b7dc57086732694e286841e0b2e9
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2023-08-02 21:36:56 +0900

    [level 2] Title: 주식가격, Time: 79.44 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -11,18 +11,19 @@
 #                 break
 #         answer.append(cnt)
 #     return answer
+
 def solution(prices):
     answer = []
-    
-    for i in range(len(prices)-1) :
-        cnt = 0;
-        for j in range(i+1, len(prices)) :
-            if (prices[i] <= prices[j]) :
-                cnt += 1;
-            if(prices[i] > prices[j]) :
-                cnt += 1;
-                break;
-        answer.append(cnt);
-    answer.append(0);
+    for i in range(len(prices)):
+        cnt = 0
+        for j in range(i+1,len(prices)):
+            if prices[i] <= prices[j]:
+                cnt += 1
+            elif prices[i] > prices[j] and i+1 != len(prices):
+                cnt += 1
+                break
+            else:
+                break
+        answer.append(cnt)
     
     return answer
