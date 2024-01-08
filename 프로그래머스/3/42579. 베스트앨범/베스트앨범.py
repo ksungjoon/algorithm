7bdd2ac3283a379ec097fcbf120d1a54b9f34753
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2024-01-08 14:49:04 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,21 @@
 from collections import defaultdict
+
 def solution(genres, plays):
-    answer = []
     genres_count = defaultdict(int)
-    genres_play = defaultdict(list)
-    i = 0
-    for g, p in zip(genres, plays):
-        genres_count[g] += p
-        genres_play[g].append((p, i))
-        i += 1
-    genres_order = sorted(genres_count, key= lambda k: -genres_count[k])
-    for g in genres_order:
-        temp = genres_play[g]
-        temp.sort(key= lambda x: (-x[0], x[1]))
-        if len(temp) > 1:
-            answer.append(temp[0][1])
-            answer.append(temp[1][1])
+    genres_list = defaultdict(list)
+    answer = []
+    for i in range(len(genres)):
+        genres_count[genres[i]]+=plays[i]
+        genres_list[genres[i]].append((i,plays[i]))
+    genres_count=sorted(genres_count,key=lambda x:genres_count[x],reverse=True)
+    
+    for genre,play in genres_list.items():
+        genres_list[genre] = sorted(play,key=lambda x:x[1],reverse=True)
+    
+    for k in genres_count:
+        if len(genres_list[k])>1:
+            answer.append(genres_list[k][0][0])
+            answer.append(genres_list[k][1][0])
         else:
-            answer.append(temp[0][1])
+            answer.append(genres_list[k][0][0])
     return answer
