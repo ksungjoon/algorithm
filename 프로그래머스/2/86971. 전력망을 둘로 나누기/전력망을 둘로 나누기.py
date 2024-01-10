91331a54a6bf684980d58f9f60bc691224c214ac
ksungjoon <122590593+ksungjoon@users.noreply.github.com>
2024-01-10 16:58:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 267.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,29 +1,8 @@
-from copy import deepcopy
-from collections import deque
-
-def bfs(arr,x,y):
-    q = deque()
-    q.append(0)
-    while q:
-        now = q.popleft()
-        y.append(now)
-        for i in range(x):
-            if arr[now][i]==1:
-                if i not in y and i not in q:
-                    q.append(i)
-    return y
 
 def solution(n, wires):
-    answer = 21e9
-    for i in range(len(wires)):
-        lst= deepcopy(wires)
-        lst.pop(i)
-        check = [[0]*n for _ in range(n)]
-        for i in range(len(lst)):
-            check[lst[i][0]-1][lst[i][1]-1] = 1
-            check[lst[i][1]-1][lst[i][0]-1] = 1
-        a = []
-        result = bfs(check,n,a)
-        if answer> abs(n-2*len(result)):
-            answer = abs(n-2*len(result))
-    return answer
+    ans = n
+    for sub in (wires[i+1:] + wires[:i] for i in range(len(wires))):
+        s = set(sub[0])
+        [s.update(v) for _ in sub for v in sub if set(v) & s]
+        ans = min(ans, abs(2 * len(s) - n))
+    return ans
